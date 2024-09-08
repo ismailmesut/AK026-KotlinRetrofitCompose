7da04572b267af0a59e3ae4c716b29832f53b23e
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-08 13:59:55 +0300

    updated: lazy column

--- app/src/main/java/com/ismailmesutmujde/kotlinretrofitcompose/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinretrofitcompose/MainActivity.kt
@@ -7,10 +7,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -19,9 +24,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -95,9 +102,30 @@ fun AppBar() {
         colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Cyan)
     )
 
-
 }
 
+@Composable
+fun CryptoList(cryptos: List<CryptoModel>) {
+    LazyColumn(contentPadding = PaddingValues(5.dp)) {
+        items(cryptos) {crypto ->
+            CryptoRow(crypto = crypto)
+        }
+    }
+}
+
+@Composable
+fun CryptoRow(crypto: CryptoModel) {
+    Column(modifier = Modifier.fillMaxWidth().background(color = MaterialTheme.colorScheme.background)) {
+        Text(text = crypto.currency,
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = crypto.price,
+            style = MaterialTheme.typography.headlineSmall,
+            modifier = Modifier.padding(2.dp))
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
